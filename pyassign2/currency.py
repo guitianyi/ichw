2d3b6f73fe7de3ddb6469276fc70acb52516f16a
guitianyi <31918771+guitianyi@users.noreply.github.com>
2017-12-02 22:18:16 +0800

    Update currency.py
    The exchange rate is fixed at that of an exact time.
    In this exchange, the user is changing amount_from money in currency currency_from to the currency currency_to. The value returned represents the amount in currency currency_to.
    The value returned has type float.
    Parameter currency_from: the currency on hand
     Precondition: currency_from is a string for a valid currency code
     Parameter currency_to: the currency to convert to
     Precondition: currency_to is a string for a valid currency code
     Parameter amount_from: amount of currency to convert
    Precondition: amount_from is a float.
    If the parameters input are not qualified to the preconditions, the result returned will be “Exchange currency code is invalid”
    The test_exchange() function uses assert() function and does three tests.
    The test_ALL() function can test all functions in this code. If the test is passed, it will print ‘test passed’.

--- pyassign2/currency.py
+++ pyassign2/currency.py
@@ -13,6 +13,16 @@ def exchange(currency_from,currency_to,amount_from):
     for x in c:
         if x in ('1','2','3','4','5','6','7','8','9','0','.'):
             d=d+x
-    d=float(d)
+    if d=='':
+        d='Exchange currency code is invalid'
+    else:
+        d=float(d)
     return d
+def test_exchange():
+    assert(exchange('USD','CNY',100)==652.615)
+    assert(exchange('BHD','MTL',100)=='Exchange currency code is invalid')
+    assert(exchange('BHD','MUR',100)==8721.8615407464)
 
+def test_ALL():
+    test_exchange()
+    print('test passed')
